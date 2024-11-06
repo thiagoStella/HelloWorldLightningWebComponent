f73f2e0f54e89e289eb4cc33606b7f4c5e04ac72
thiagoStella <thiagostellapontes01@gmail.com>
2024-11-06 14:52:34 -0300

    Botao mostrar e esconder

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,7 +2,17 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     variavel = 'World';
+    clickedButtonMostrarNome;
+
     changeHandler(event) {
     this.variavel = event.target.value;
+    }
+
+    handleClick(event) {
+        this.clickedButtonMostrarNome = this.variavel;
+    }
+    
+    handleClick2(event) {
+        this.clickedButtonMostrarNome = '';
     }
 }
